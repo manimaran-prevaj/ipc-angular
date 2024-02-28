88ba73464451af1e8b71b8f36734d69414c1ea37
Saugata Roy <sroy@pizzapizza.ca>
2024-02-28 14:12:17 -0500

    Adding angualr SSR, creating CI shell scripts and bitbucket pipeline YAML

---
+++ CI/build-all.sh
@@ -0,0 +1,6 @@
+# Not used in CI
+# Only debugging
+
+sh CI/build.sh web-en
+sh CI/build.sh server-en
+sh CI/build.sh ssr-server
---
+++ src/revision.ts
@@ -0,0 +1,4 @@
+export const buildRevision = '$DATE_STR, Angular: $PV_ANGULAR, Build: $BITBUCKET_BUILD_NUMBER';
+export const buildNumber = '$BITBUCKET_BUILD_NUMBER';
+
+

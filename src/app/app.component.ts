fb6efed327215d261d923504e5c8118125b0698b
Saugata Roy <sroy@pizzapizza.ca>
2024-03-05 14:13:59 -0500

    Test component for deployment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 	title = 'pizza-pizza-ccc';
+	date = new Date();
+	fullDate = this.date.toLocaleDateString('en-us', { weekday: "long", year: "numeric", month: "long", day: "numeric" });
+	time = this.date.toLocaleTimeString('en-us');
+	dateTime = this.fullDate + ' ' + this.time;
 }

2183f9986652080c37edc0b1cc78d87155bd8074
Saugata Roy <sroy@pizzapizza.ca>
2024-04-16 00:12:48 -0400

    Customer entry: Customer details form

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,17 +2,46 @@ import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
 	imports: [
 		MatCardModule,
 		MatSidenavModule,
-		MatExpansionModule
+		MatExpansionModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatDatepickerModule,
+		MatButtonToggleModule
 	],
 	exports: [
 		MatCardModule,
 		MatSidenavModule,
-		MatExpansionModule
+		MatExpansionModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatButtonModule,
+		MatCheckboxModule,
+		MatDatepickerModule,
+		MatButtonToggleModule
+	],
+	providers: [
+		{
+			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+			useValue: {
+				subscriptSizing: 'dynamic',
+				appearance: 'fill',
+			}
+		},
+		provideNativeDateAdapter()
 	],
 })
 
--- src/app/new-customer/components/customer-details/customer-details.component.ts
+++ src/app/new-customer/components/customer-details/customer-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
 	selector: 'app-customer-details',
@@ -6,6 +7,29 @@ import { Component } from "@angular/core";
 	styleUrls: ['./customer-details.component.scss'],
 })
 
-export class CustomerDetailsComponent {
+export class CustomerDetailsComponent implements OnInit {
+	public customerDetailsForm: FormGroup;
+	constructor(
+		private formBuilder: FormBuilder
+	) { }
 
+	ngOnInit() {
+		this.customerDetailsForm = this.formBuilder.group({
+			firstName: ["", [Validators.required]],
+			lastName: ["", [Validators.required]],
+			email: ["", [Validators.required, Validators.email]],
+			phone: ["", [Validators.required, Validators.maxLength(10), Validators.minLength(10)]],
+			phoneExt: [""],
+			datePicker: ["", [Validators.required]],
+			modeOfDelivery: ["delivery", [Validators.required]]
+		});
+	}
+
+	onlyNumber(event) {
+		let ASCIICode = (event.which) ? event.which : event.keyCode
+		if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) {
+			return false;
+		}
+		return true;
+	}
 }

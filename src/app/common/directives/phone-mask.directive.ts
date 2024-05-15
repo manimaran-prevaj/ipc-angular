6c74a22401a81f7eff19e8dff6ee1c47e542373d
Saugata Roy <sroy@pizzapizza.ca>
2024-05-15 12:35:39 -0400

    UI alignment changes for dwelling type options

--- src/app/common/directives/phone-mask.directive.ts
+++ src/app/common/directives/phone-mask.directive.ts
@@ -1,10 +1,10 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, OnDestroy } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
 	selector: '[formControlName][appPhoneMask]',
 })
-export class CCCPhoneMaskDirective {
+export class CCCPhoneMaskDirective implements OnDestroy {
 
 	constructor(public ngControl: NgControl) { }
 
@@ -20,23 +20,34 @@ export class CCCPhoneMaskDirective {
 
 
 	onInputChange(event, backspace) {
-		let newVal = event.replace(/\D/g, '');
-		if (backspace && newVal.length <= 6) {
-			newVal = newVal.substring(0, newVal.length - 1);
-		}
-		if (newVal.length === 0) {
-			newVal = '';
-		} else if (newVal.length <= 3) {
-			newVal = newVal.replace(/^(\d{0,3})/, '($1)');
-		} else if (newVal.length <= 6) {
-			newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) $2');
-		} else if (newVal.length <= 10) {
-			newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
-		} else {
-			newVal = newVal.substring(0, 10);
-			newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
-		}
+		if (event) {
+			let newVal = event.replace(/\D/g, '');
+			if (backspace && newVal.length <= 6) {
+				newVal = newVal.substring(0, newVal.length - 1);
+			}
+			if (newVal.length === 0) {
+				newVal = '';
+			} else if (newVal.length <= 3) {
+				newVal = newVal.replace(/^(\d{0,3})/, '($1)');
+			} else if (newVal.length <= 6) {
+				newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '($1) $2');
+			} else if (newVal.length <= 10) {
+				newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+			} else {
+				newVal = newVal.substring(0, 10);
+				newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '($1) $2-$3');
+			}
 
-		this.ngControl.valueAccessor?.writeValue(newVal);
+			this.ngControl.valueAccessor?.writeValue(newVal);
+		}
+	}
+
+	ngOnDestroy(): void {
+		this._removeHostListeners();
+	}
+
+	private _removeHostListeners(): void {
+		window.removeEventListener('ngModelChange', this.onModelChange);
+		window.removeEventListener('keydown.backspace', this.keydownBackspace);
 	}
 }
